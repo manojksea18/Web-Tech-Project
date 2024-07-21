c53e7d23222188223be04b6350b5d4cfa2d52186
Manoj Khati <mksea18@gmail.com>
2024-07-21 21:46:42 +0545

    database connected

---
+++ Form.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Form</title>
+    <style>
+    </style>
+</head>
+
+<body>
+    <?php
+    //to connect db and instert data to db
+    $name = $_POST['name'];
+    $pword = $_POST['password'];
+    $email = $_POST['email'];
+    $gender = $_POST['gender'];
+    $country = $_POST['country'];
+    $servername = "localhost";
+    $username = "username";
+    $password = "";
+    $db = "dbhcoe";
+
+
+    ?>
+</body>
